2299d5214b404f71c8876a45c01734aa0e80c668
sist <sist@DESKTOP-240M8SR>
2023-06-02 09:06:47 +0900

    [feat] ZNDK-8 온라인문의 파일 전송 완료

--- FinalProject_GotGongBang/src/main/java/com/spring/gotgongbang/board/model/InterBoardDAO.java
+++ FinalProject_GotGongBang/src/main/java/com/spring/gotgongbang/board/model/InterBoardDAO.java
@@ -2,7 +2,10 @@ package com.spring.gotgongbang.board.model;
 
 public interface InterBoardDAO {
 	
-	// 온라인 문의 완료
+	// 파일 첨부 기능이 없는 온라인 문의 데이터 넣기
 	int add(InquiryVO iqvo);
+	
+	// 파일 첨부 기능이 있는 온라인 문의
+	int add_withFile(InquiryVO iqvo);
 
 }
