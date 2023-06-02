692d46f9c864be2920a27897b8cfcc960ce7542b
JuneYub <ajsjdlwj0123@gmail.com>
2023-06-02 16:00:18 +0900

    [feat] ZNDK-4 상세 이미지 관련 vo 추가

---
+++ FinalProject_GotGongBang/src/main/java/com/spring/gotgongbang/order/model/DetailImgVO.java
@@ -0,0 +1,27 @@
+package com.spring.gotgongbang.order.model;
+
+public class DetailImgVO {
+	private int detail_img_num_pk;
+	private int order_num_fk;
+	private String detail_img_name;
+	public int getDetail_img_num_pk() {
+		return detail_img_num_pk;
+	}
+	public void setDetail_img_num_pk(int detail_img_num_pk) {
+		this.detail_img_num_pk = detail_img_num_pk;
+	}
+	public int getOrder_num_fk() {
+		return order_num_fk;
+	}
+	public void setOrder_num_fk(int order_num_fk) {
+		this.order_num_fk = order_num_fk;
+	}
+	public String getDetail_img_name() {
+		return detail_img_name;
+	}
+	public void setDetail_img_name(String detail_img_name) {
+		this.detail_img_name = detail_img_name;
+	}
+	
+	
+}
