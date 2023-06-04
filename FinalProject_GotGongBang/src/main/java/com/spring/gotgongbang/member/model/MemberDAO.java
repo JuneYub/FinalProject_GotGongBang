2f29b4aff3f3f000fc1efca218280dbaebd72368
HongYongHun98 <ghddydgns3@naver.com>
2023-06-05 01:44:40 +0900

    [feat] ZNDK-7 회원가입 폼 수정 및 이메일, 아이디 중복확인과 암호화 구현

--- FinalProject_GotGongBang/src/main/java/com/spring/gotgongbang/member/model/MemberDAO.java
+++ FinalProject_GotGongBang/src/main/java/com/spring/gotgongbang/member/model/MemberDAO.java
@@ -1,9 +1,12 @@
 package com.spring.gotgongbang.member.model;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class MemberDAO implements InterMemberDAO {
@@ -17,4 +20,20 @@ public class MemberDAO implements InterMemberDAO {
 		sqlsession.insert("member.insertMember", membervo);
 	}
 
+	// 이메일 중복 확인 AJAX 요청 처리
+	@Override
+	public int isEmailDuplicate(String email) {
+		int n = sqlsession.selectOne("member.isEmailDuplicate", email);
+		return n;
+	}
+
+	// 아이디 중복 확인 AJAX 요청 처리
+	@Override
+	public int isIdDuplicate(String id) {
+		int n = sqlsession.selectOne("member.isIdDuplicate", id);
+		return n;
+	}
+
+	
+	
 }
